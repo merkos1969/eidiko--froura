518e8fc9a72b3fc0dc0fa1290c0a08b87250169d
merkos1969 <merkos1969@gmail.com>
2026-05-12 16:03:10 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,44 @@
-const CACHE_NAME = 'eidiko-froura-v90';
-
-const urlsToCache = [
+const CACHE_NAME = 'eidiko-froura-v101';
+const URLS_TO_CACHE = [
   './',
   './index.html',
   './style.css',
   './app.js',
+  './TAX_TABLE.js',
   './data.js',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './logo_poyef.png',
   './splash.png',
   './splash_fullscreen.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve())
+      )
     ).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
     fetch(event.request)
-      .then(response => {
+      .then((response) => {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
       })
       .catch(() => caches.match(event.request))
--- sw.js
+++ sw.js
@@ -1,22 +1,46 @@
-const CACHE_NAME = 'froura-cache-v1';
+const CACHE_NAME = 'eidiko-froura-v101';
 const URLS_TO_CACHE = [
+  './',
   './index.html',
-  './styles.css',
+  './style.css',
   './app.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './TAX_TABLE.js',
+  './data.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './logo_poyef.png',
+  './splash.png',
+  './splash_fullscreen.png'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve())
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
